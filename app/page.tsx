b52e50ef9c48e5ce67c18247737e5fe9c31b230d
Nikolai Bik <voowoou@gmail.com>
2024-03-20 00:06:14 +0300

    Implement provider extension checking

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,27 @@
+'use client';
+
+import { useEffect } from 'react';
 import Network from './components/Network';
 import Wallet from './components/Wallet';
 import Transaction from './components/Transaction';
+import './page.css';
 
-const App = () => {
+const page = () => {
+  let injectedProvider = false; // Наличие/отсутствие провайдера
+
+  if (typeof window.ethereum !== 'undefined') {
+    injectedProvider = true;
+    console.log(window.ethereum);
+  }
+
+  const isMetaMask = injectedProvider ? window.ethereum.isMetaMask : false; // Проверка наличия Metamask в качестве провайдера
+
   return (
-    <>
-      <Network />
-      <Wallet />
-      <Transaction />
-    </>
+    <div className="App">
+      <h2>Injected Provider {injectedProvider ? 'DOES' : 'DOES NOT'} Exist</h2>
+      {isMetaMask && <button>Connect MetaMask</button>}
+    </div>
   );
 };
 
-export default App;
+export default page;
